d4028640a4e909443cafe957f3af11006b77aa5a
Artur Lobanov <artur.lobanov@desy.de>
2015-01-16 19:56:09 +0100

    Update loop script

--- CMGToolAna/macros/LoopDir.py
+++ CMGToolAna/macros/LoopDir.py
@@ -3,26 +3,43 @@
 import sys
 from ROOT import *
 
-file1 = TFile.Open('../subEl/Output/CMG_MC_WJets_his.root')
-file2 = TFile.Open('../subEl/Output/CMG_MC_T1tttt_1200_800_his.root')
+file1 = TFile.Open('../subEl/Output/CMG_MC_WJets_his.root','READ')
+file2 = TFile.Open('../subEl/Output/CMG_MC_TTbar_his.root','READ')
+#file3 = TFile.Open('../subEl/Output/CMG_MC_T1tttt_1200_800_his.root','READ')
 
 outfile  = TFile("StackPlots.root", "recreate")
 
 def findHisto(file,name):
     for cutKey in file.GetListOfKeys():
-        histList = cutKey.ReadObj()
-
         if cutKey.IsFolder() == 1:
-            for histKey in histList.GetListOfKeys():
+            histDir = cutKey.ReadObj()
+            for histKey in histDir.GetListOfKeys():
                 if(histKey.GetName() == name):
                     return histKey.ReadObj()
     else:
         return 0
 
-hist1 = findHisto(file1,"HT_1")
-hist2 = findHisto(file2,"HT_1")
+def copy(infile,outfile):
+    indirlist = infile.GetListOfKeys()
+    outdirlist = outfile.GetListOfKeys()
 
-c1=TCanvas('c1','',800,600)
+    for cutKey in indirlist:
+
+        if cutKey.IsFolder() == 1:
+            histDir = cutKey.ReadObj()
+
+            # create same folder
+            #if(outfile)
+            outfile.mkdir(histDir.GetName())
+
+#copy(file1, outfile)
+
+histname = 'HT_1'
+
+hist1 = findHisto(file1,histname)
+hist2 = findHisto(file2,histname)
+
+c1=TCanvas(histname,hist1.GetTitle(),800,600)
 hist1.Draw("hist")
 hist2.Draw("hist same")
 
