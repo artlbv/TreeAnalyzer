2746d7ac1b4b74a2e40ca33afd7ff43379ce65e3
Artur Lobanov <artur.lobanov@desy.de>
2015-03-23 14:33:40 +0100

    Optimize runSample

--- CMGToolAna/python/runSample.py
+++ CMGToolAna/python/runSample.py
@@ -5,35 +5,47 @@ import os
 from glob import glob
 from sys import exit
 
+'''
+# load root in batch mode
+sysarg = sys.argv
+sys.argv = ['-b']
 from ROOT import gROOT
 from ROOT import TFile
+sys.argv = sysarg
+'''
 
 def help():
     print 'First argument analysis:'
-    print './runreader.py SampleDir'
+    print './runreader.py SampleDir (with tree,tth etc. structure)'
     sys.exit(0)
 
 def GetNevents(loc):
+# get full number of events generated
     EvtFile = open(loc+"ttHLepSkimmer/events.txt", "r")
-    theInts = []
-    for val in EvtFile.read().split():
-        if val.isdigit():
-            theInts.append(val)
-        EvtFile.close()
-    return float(theInts[0])
 
+    for line in EvtFile.readlines():
+        if 'all events' in line:
+            evts = line.split()[2]
+            break
 
-# Settings
-treename = 'treeProducerSusySingleLepton'
-lumi = 1000 #pb-1
-# choose the analysis and a sample
-exe = 'TreeAnalyzer_SingleEl.exe'
+    return int(evts)
 
 # Get sample directory
 if len(sys.argv) > 1:
     sampDir = sys.argv[1]
 else:
     help()
+
+# Get current script dir
+pyDir = sys.argv[0]
+cwd = os.path.dirname(pyDir)
+
+# Settings
+treename = 'treeProducerSusySingleLepton'
+lumi = 1000 #pb-1
+exe = 'TreeAnalyzer_SingleEl.exe'
+# get relative path
+exe = cwd+'/../'+exe
 
 # Calculate number of events
 entries = GetNevents(sampDir+'/')
@@ -44,10 +56,10 @@ sample = os.path.basename(sampDir.rstrip('//'))
 if 'MC' in sampDir: scene = 'MC'
 else: scene = 'Data'
 
-print sampDir, 'has', entries, 'entries for sample', scene, '_', sample
+print sampDir, 'has', entries, 'entries for sample', scene+'_'+sample
 
 # NB! weight is lumi/entries
 fileName = sampDir+'/'+treename+'/ '+str(lumi/entries)+' '
 
-print([".././"+exe, fileName,scene+'_'+sample])
-os.system(".././"+exe+" "+fileName+" "+scene+'_'+sample)
+print([exe, fileName,scene+'_'+sample])
+os.system(exe+" "+fileName+" "+scene+'_'+sample)
