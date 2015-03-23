da2e1d664a4108f7edee29e7c07554762720ad49
Artur Lobanov <artur.lobanov@desy.de>
2015-03-23 13:10:54 +0100

    Clean up runSample

--- CMGToolAna/python/runSample.py
+++ CMGToolAna/python/runSample.py
@@ -8,9 +8,6 @@ from sys import exit
 from ROOT import gROOT
 from ROOT import TFile
 
-# default samples location
-locSamp = 'DESY'
-
 def help():
     print 'First argument analysis:'
     print './runreader.py SampleDir'
@@ -25,19 +22,11 @@ def GetNevents(loc):
         EvtFile.close()
     return float(theInts[0])
 
-def GetTreeName(file):
-    keylist = file.GetListOfKeys()
-    treeKey = keylist.At(0)
-    treeName = treeKey.GetName()
 
-    if 'tree' in treeName:
-        return treeName
-    else:
-        print 'Tree not found in ', file.GetName()
-        exit(0)
-
+# Settings
+treename = 'treeProducerSusySingleLepton'
+lumi = 1000 #pb-1
 # choose the analysis and a sample
-srcdir = '../src/'
 exe = 'TreeAnalyzer_SingleEl.exe'
 
 # Get sample directory
@@ -48,33 +37,17 @@ else:
 
 # Calculate number of events
 entries = GetNevents(sampDir+'/')
-
-treename = 'treeProducerSusySingleLepton'
-lumi = 1000 #pb-1
 #remove the trailing / : rstrip('//')
 sample = os.path.basename(sampDir.rstrip('//'))
 
+# get scenario MC/Data
 if 'MC' in sampDir: scene = 'MC'
 else: scene = 'Data'
 
 print sampDir, 'has', entries, 'entries for sample', scene, '_', sample
 
+# NB! weight is lumi/entries
 fileName = sampDir+'/'+treename+'/ '+str(lumi/entries)+' '
 
 print([".././"+exe, fileName,scene+'_'+sample])
 os.system(".././"+exe+" "+fileName+" "+scene+'_'+sample)
-
-'''
-print "cross section x lumi", xsec_lumi[samp][i], "Events generated", entries
-fileNames+=inDir[scene][samp]+dirsHT[samp][i]+treename+' '+str(xsec_lumi[samp][i]/entries)+' '
-
-                # process
-                print "file name to be processed", fileNames
-                print "Sample:", samp,scene
-                if exe == ' ':
-                    print fileNames,samp,scene
-                    reader(fileNames,scene+'_'+samp)
-                else:
-                    print([".././"+exe, fileNames,scene+'_'+samp])
-                    os.system(".././"+exe+" "+fileNames+" "+scene+'_'+samp)
-'''
